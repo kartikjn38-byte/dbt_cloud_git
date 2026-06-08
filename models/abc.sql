d41b7ca358a2d2ce6dc9677eec6707392055675e
kartikjn38@gmail.com <kartikjn38@gmail.com>
2026-06-08 11:46:27 +0000

    first commit

---
+++ models/abc.sql
@@ -0,0 +1,5 @@
+select 'kartik' as first_name ,'Jain' as last_name , '31' as age
+UNION ALL
+select 'navita' as first_name ,'Jain' as last_name , '30' as age
+UNION ALL
+select 'hemant' as first_name ,'Jain' as last_name , '55' as age
